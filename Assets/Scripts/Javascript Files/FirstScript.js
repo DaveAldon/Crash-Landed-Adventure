aa4be9d057681f2f97b2de1b1239f889f034028d
Pierce Ortlieb <ortliebp@mail.gvsu.edu>
2016-06-17 16:47:53 -0400

    Started manipulating the script that follows objects

--- Assets/Scripts/Javascript Files/FirstScript.js
+++ Assets/Scripts/Javascript Files/FirstScript.js
@@ -1,10 +1,20 @@
 ﻿#pragma strict
 
  var target : Transform;
- var distance : float = 10;
+ var xDistance : float -1.5;
+ var yDistance : float 0.3;
+ var zDistance : float -7.75; 
+
+ var xRotation : float 14.5;
+ var yRotation : float -2.0;
+ var zRotation : float 0.12;
 
  function Update() {
-     transform.position.z = target.position.z -distance;
-     transform.position.y = target.position.y;
-     transform.position.x = target.position.x;
+     transform.position.z = (target.position.z + zDistance);
+     transform.position.y = (target.position.y + yDistance);
+     transform.position.x = (target.position.x + xDistance);
+
+     transform.rotation.x = xRotation;
+     transform.rotation.y = yRotation;
+     transform.rotation.z = zRotation;      
  }
